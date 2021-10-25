b1e96f09610834e0ef84509d62195a8dbf4e48f9
JorgeSTriana <jorgetriana1951@gmail.com>
2021-10-25 16:32:06 -0500

    Heroku four push

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,4 @@
-const jwt = require(jsonwebtoken)
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const msg = required('../helpers/messages')
